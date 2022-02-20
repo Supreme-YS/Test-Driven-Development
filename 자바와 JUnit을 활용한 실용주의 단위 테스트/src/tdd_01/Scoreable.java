3df67caed01098fade9e9299bbe29ae269767e01
Supreme-YS <dudtjr1225@gmail.com>
2022-02-20 21:51:48 +0900

    Update

---
+++ 자바와 JUnit을 활용한 실용주의 단위 테스트/src/tdd_01/Scoreable.java
@@ -0,0 +1,10 @@
+package tdd_01;
+
+/**
+ * @FuntionalInterface
+ * 구현해야 할 메서드가 단 하나이면, FunctionalInterface이다.
+ */
+@FunctionalInterface
+public interface Scoreable {
+    int getScore();
+}
---
+++ 자바와 JUnit을 활용한 실용주의 단위 테스트/test/tdd_01/ScoreCollectionTest.java
@@ -0,0 +1,21 @@
+package tdd_01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.
+class ScoreCollectionTest {
+
+    @Test
+    public void 덧셈_테스트를_해보자() {
+        // 준비
+        ScoreCollection scoreCollection = new ScoreCollection();
+        scoreCollection.add(() -> 5);
+        scoreCollection.add(() -> 7);
+
+        // 실행
+        int result = scoreCollection.arithmeticMean();
+
+        // 단언
+        assertThat
+    }
+}
